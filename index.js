5d7ee012bf2675f52779d026cb52ec586b2cf6c4
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2018-02-27 14:56:32 -0500

    cleanup and addition of faker

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 // index.js
 const sha1 = require('sha1')
+var faker = require('faker')
 
 module.exports = () => {
   const ROLES = ['Super Admin', 'Agency Admin', 'Agency User']
   const AGENCIES = ['Starcom', 'Spark Foundry', 'Zenith', 'Blue 449']
 
-
   class User {
-    constructor (id, firstname, lastname, email, role, lionlogin, agency) {
+    constructor(id, firstname, lastname, email, role, lionlogin, agency) {
+      const name = faker.name.findName()
+      const first = name.split(' ')[0]
+      const last = name.split(' ')[1]
       const struct = {
         id: id || null,
-        firstname: firstname || '',
-        lastname: lastname || '',
-        email: email || `${sha1((new Date()).getTime()).substring(0, 3)}@publicisgroupe.net`,
-        role: ROLES[Math.floor(Math.random() * ROLES.length) + 1],
-        lionlogin: `${sha1((new Date()).getTime()).substring(0, 6)}`,
-        agency: AGENCIES[Math.floor(Math.random() * AGENCIES.length) + 1]
+        firstname: firstname || first,
+        lastname: lastname || last,
+        email: email || `${first.toLowerCase()}.${last.toLowerCase()}@publicisgroupe.net`,
+        role: ROLES[Math.floor(Math.random() * ROLES.length)],
+        lionlogin: `${first.substring(0, 1).toLowerCase()}${last.substring(0, 7).toLowerCase()}`,
+        agency: AGENCIES[Math.floor(Math.random() * AGENCIES.length)]
       }
 
       return struct
@@ -27,7 +30,7 @@ module.exports = () => {
   }
 
   for (let i = 1; i < 100; i++) {
-    const user = new User(i, `First ${i}`, `Last ${i}+1`)
+    const user = new User(i)
     data.users.push(user)
   }
 
